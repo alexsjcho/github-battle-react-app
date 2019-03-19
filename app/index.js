081ef7ded55bec01291845ac11897d793a3b25b2
Alex Cho <alexsjcho@gmail.com>
2019-03-18 20:30:30 -0700

    covered babel, webpack, and react

--- app/index.js
+++ app/index.js
@@ -1,13 +1,11 @@
-var React = require ('react');
+var React = require("react");
+var ReactDOM = require("react-dom");
+require("./index.css");
 
-class App extends React Component {
-    render(){
-        return (
-            <div>
-                <h1>Hello World!</h1>
-            </div>
-        )
-    }
+class App extends React.Component {
+  render() {
+    return <div>Hello World!</div>;
+  }
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById("app"));
